77fe87494fbdfff4748fdf005da3f70208ea6115
FND <fnd@localhost.localdomain>
2022-02-10 15:25:03 +0100

    done

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ let CONFIG = {
 		urls: ["stun:stun.1.google.com:19302"]
 	}]
 };
+let ID = Date.now().toString(36) + Math.random().toString(36).substr(2);
 
-let PEER_A = document.querySelector("button[data-type=peer-a]");
-let PEER_B = document.querySelector("button[data-type=peer-b]");
+let REGISTER = document.querySelector("button");
 let CLIPBOARD = document.querySelector("button[data-type=clipboard]");
 let TEXT = {
 	_el: document.querySelector("textarea"),
 	set value(obj) {
+		obj = {
+			...obj,
+			candidates: FND.candidates
+		};
 		this._el.value = JSON.stringify(obj, null, 4);
 	},
 	get value() {
 		try {
 			return JSON.parse(this._el.value);
 		} catch(err) {
-			return null;
+			return {};
 		}
 	},
 	get raw() {
@@ -25,31 +29,38 @@ let TEXT = {
 };
 let LOG = document.querySelector("pre");
 
-let CONN = establishConnection();
-globalThis.FND = { conn: CONN };
+globalThis.FND = {};
+let CONN = FND.conn = establishConnection();
 
-PEER_A.addEventListener("click", async ev => {
-	if(!TEXT.value) {
+REGISTER.addEventListener("click", async ev => {
+	let { offer, answer, candidates = [] } = TEXT.value;
+	if(answer) {
+		log("[PEER A] processing answer");
+		answer = new RTCSessionDescription(answer);
+		CONN.setRemoteDescription(answer);
+	} else if(offer) {
+		log("[PEER B] processing offer");
+		let offer = new RTCSessionDescription(TEXT.value.offer);
+		CONN.setRemoteDescription(offer);
+
+		let answer = await CONN.createAnswer();
+		CONN.setLocalDescription(answer);
+		TEXT.value = { answer };
+	} else if(!CONN.localDescription) {
+		let chan = CONN.createDataChannel("sendChannel");
+		registerChannel(chan);
+
 		log("[PEER A] creating offer");
 		let offer = await CONN.createOffer();
 		CONN.setLocalDescription(offer);
-		TEXT.value = offer;
+		TEXT.value = { offer };
 		return;
 	}
 
-	log("[PEER A] processing answer");
-	let answer = new RTCSessionDescription(TEXT.value);
-	CONN.setRemoteDescription(answer);
-});
-
-PEER_B.addEventListener("click", async ev => {
-	log("[PEER B] processing offer");
-	let offer = new RTCSessionDescription(TEXT.value);
-	CONN.setRemoteDescription(offer);
-
-	let answer = await CONN.createAnswer();
-	CONN.setLocalDescription(answer);
-	TEXT.value = answer;
+	for(let can of candidates) {
+		log("[PEER] registering candidate");
+		CONN.addIceCandidate(new RTCIceCandidate(can));
+	}
 });
 
 CLIPBOARD.addEventListener("click", async ev => {
@@ -63,11 +74,40 @@ CLIPBOARD.addEventListener("click", async ev => {
 	}, 1000);
 });
 
+function registerChannel(chan) {
+	chan.addEventListener("open", ev => {
+		console.log("[DATA] open");
+		chan.send(`hello from ${ID}`);
+	});
+	chan.addEventListener("close", ev => {
+		console.log("[DATA] close");
+	});
+	chan.addEventListener("message", ev => {
+		let { data } = ev;
+		console.log("[DATA] message", data);
+		if(data.startsWith("hello from ")) {
+			let id = data.substr(11);
+			chan.send(`hi, ${id}, I'm ${ID}`);
+		}
+	});
+	FND.chan = chan;
+}
+
 function establishConnection() {
 	let conn = new RTCPeerConnection(CONFIG);
 
+	let candidates = []
 	conn.addEventListener("icecandidate", ev => {
-		console.log("[ICE]", ev.candidate);
+		let { candidate } = ev;
+		console.log("[ICE]", candidate);
+		if(candidate) {
+			candidates.push(candidate);
+		} else {
+			console.log("[ICE]", candidates);
+			FND.candidates = candidates;
+		}
+
+		TEXT.value = TEXT.value; // XXX: hacky
 	});
 
 	conn.addEventListener("iceconnectionstatechange", ev => {
@@ -79,15 +119,8 @@ function establishConnection() {
 	});
 
 	conn.addEventListener("datachannel", ev => {
-		console.log("[DATA]", ev.channel);
-
-		let receiveChannel = event.channel;
-		receiveChannel.onopen = ev => {
-			console.log("[DATA] channel is ready");
-		};
-		receiveChannel.message = ev => {
-			console.log("[DATA] message", ev);
-		};
+		console.log("[DATA] channel", ev.channel);
+		registerChannel(ev.channel);
 	});
 
 	return conn;
